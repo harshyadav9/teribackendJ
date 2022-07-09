6e777e9fe12e24bc33b46e8c0f9abb03ef8fc847
Chestha <harshy110@gmail.com>
2022-07-10 01:16:55 +0530

    changed port

--- backend/src/main/java/com/exam/backend/controller/TerryController.java
+++ backend/src/main/java/com/exam/backend/controller/TerryController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/terry/olympiad")
+@RequestMapping("/terry")
 @CrossOrigin(origins = "*")
 public class TerryController {
 
